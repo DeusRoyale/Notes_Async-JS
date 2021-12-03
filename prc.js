faa85874a7953db3b182cb9aeef3b8306236da1d
DeusRoyale <durjoyr369@gmail.com>
2021-12-03 18:06:12 +0600

    !!!

--- prc.js
+++ prc.js
@@ -1,10 +1,5 @@
-function isPangram(string) {
-  return "abcdefghijklmnopqrstuvwxyz"
-    .toLowerCase()
-    .split('')
-    .every((x) => string.includes(x))
-    ? console.log('true')
-    : console.log('false')
-}
-
-isPangram("A quick brown fox jumps over the lazy dog")
+/* 
+.split()
+.every()
+.includes
+*/
