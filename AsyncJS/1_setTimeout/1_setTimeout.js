31b48619fd1e8a6b86adca3e714c473349bf4441
DeusRoyale <durjoyr369@gmail.com>
2021-12-08 11:37:54 +0600

    2

--- AsyncJS/1_setTimeout/1_setTimeout.js
+++ AsyncJS/1_setTimeout/1_setTimeout.js
@@ -7,3 +7,19 @@
 ***Code before and after it don't have access to 'it'.
 
 */
+
+let timeExpired = false
+const expiresIn = 3500
+
+setTimeout(() => {
+  console.log(`Time's up!`)
+  timeExpired = true
+}, expiresIn)
+
+window.handleCompleted = () => {
+  if(timeExpired){
+    alert(`Time's up tin can!`)
+  }
+  
+  alert(`Welcome, fellow human!`)
+}
